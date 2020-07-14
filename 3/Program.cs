4cabb7250eda89cd56ade2e6c39d415cbdc3f77c
NaI <fd4124983399@gmail.com>
2020-07-15 00:17:57 +0800

    3

---
+++ 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using  System.Collections.Generic;
+
+/*
+ * @lc app=leetcode id=3 lang=csharp
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int maxLength = 0;
+        int currentLength = 0;
+
+        Dictionary<char, int> state = new Dictionary<char, int>();
+
+        for (int i=0;i<s.Length;i++){
+           if (!state.ContainsKey(s[i])){
+                state[s[i]] = i;
+                currentLength++;
+            }
+            else{
+                if (currentLength > maxLength){
+                    maxLength = currentLength;
+                }
+
+                i = state[s[i]];
+                state.Clear();
+                currentLength = 0;
+            }
+        }
+
+        if (currentLength > maxLength){
+            maxLength = currentLength;
+        }
+
+        return maxLength;
+    }
+
+    // public static void Main(){
+    //     Solution sol = new Solution();
+    //     string input = " ";
+    //     System.Console.WriteLine(sol.LengthOfLongestSubstring(input));
+    // }
+}
+// @lc code=end
+
